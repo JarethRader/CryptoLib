5983946d8fdb3d3eb6467fe2a9b8bbddcee3a236
Jareth Rader <j.rader175@gmail.com>
2019-12-06 00:37:54 -0800

    patch 1.3 (#10)
    * Updated bug crashign the site if metamask wasn't installed (#7)
    
    * Fixed bug where not having redux devtools installed would crash the app (#9)

--- server.js
+++ server.js
@@ -45,6 +45,14 @@ if (process.env.NODE_ENV == "production") {
   app.get("*", () => (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
 }
 
 app.listen(port, () => {

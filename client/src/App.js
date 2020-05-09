b3ef25f3e52b92b4a596851205366c8542a55850
Jareth Rader <j.rader175@gmail.com>
2020-05-09 09:06:01 -0400

    V0.1.11 (#42)
    * V0.1.9 (#39)
    
    * Changing PDF viewer
    
    * Updated UI
    
    * Updated UI
    
    * V0.1.11 (#41)
    
    * Changing PDF viewer
    
    * Updated UI
    
    * Updated UI
    
    * Fixed some routing bugs and web3 isntance bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
 //Import Dependencies
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import store from "./store";
-import { getMetamaskAddress, loadUser } from "./actions/userAction";
-import { Helmet } from "react-helmet";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import store from './store';
+import { getMetamaskAddress, loadUser } from './actions/userAction';
+import { Helmet } from 'react-helmet';
 
 //Page Routes and components/modals
-import NavBar from "./components/navbar";
-import Footer from "./components/footer";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./routes/home";
-import About from "./routes/about";
-import Catalog from "./routes/catalog";
-import User from "./routes/user";
-import DailyShelf from "./routes/dailyShelf";
-import ErrorModal from "./components/errorModal";
+import NavBar from './components/navbar';
+import Footer from './components/footer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './routes/home';
+import About from './routes/about';
+import Catalog from './routes/catalog';
+import User from './routes/user';
+import DailyShelf from './routes/dailyShelf';
+import ErrorModal from './components/errorModal';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    window.ethereum.autoRefreshOnNetworkChange = false;
-    window.ethereum.on("accountsChanged", function (accounts) {
-      // Time to reload your interface with accounts[0]!
-      store.dispatch(getMetamaskAddress(accounts[0]));
-    });
+    if (window.ethereum || window.web3) {
+      window.ethereum.autoRefreshOnNetworkChange = false;
+      window.ethereum.on('accountsChanged', function (accounts) {
+        // Time to reload your interface with accounts[0]!
+        store.dispatch(getMetamaskAddress(accounts[0]));
+      });
+    }
     try {
       store.dispatch(loadUser());
     } catch (err) {
@@ -37,30 +39,30 @@ class App extends Component {
     return (
       <div>
         <Helmet>
-          <meta charSet="utf-8" />
-          <meta name="Application" content="Cryptolibs" />
+          <meta charSet='utf-8' />
+          <meta name='Application' content='Cryptolibs' />
           <title>CryptoLib</title>
-          <link rel="canonical" href="https://cryptolib.co" />
+          <link rel='canonical' href='https://cryptolib.co' />
         </Helmet>
         <BrowserRouter>
-          <div className="app">
+          <div className='app'>
             <NavBar />
-            <div className="body">
+            <div className='body'>
               <ErrorModal />
               <Switch>
-                <Route exact path="/user">
+                <Route exact path='/user'>
                   <User />
                 </Route>
-                <Route exact path="/catalog">
+                <Route exact path='/catalog'>
                   <Catalog />
                 </Route>
-                <Route exact path="/dailyShelf">
+                <Route exact path='/dailyShelf'>
                   <DailyShelf />
                 </Route>
-                <Route exact path="/about">
+                <Route exact path='/about'>
                   <About />
                 </Route>
-                <Route path="/">
+                <Route path='/'>
                   <Home />
                 </Route>
               </Switch>
